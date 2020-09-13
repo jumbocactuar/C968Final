c427e3e1ae6255547a824a687446f473bdd080d6
caburkholz@gmail.com <caburkholz@gmail.com>
2020-09-12 17:24:03 -0700

    Added Inventory, Part, and Product classes

--- C968FinalProject/C968FinalProject/Program.cs
+++ C968FinalProject/C968FinalProject/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.ComponentModel;
+// FIXME: Does System.ComponentModel need to be in all the files?
 namespace C968FinalProject
 {
     static class Program
@@ -18,5 +19,51 @@ namespace C968FinalProject
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new mainScreenForm());
         }
+    }
+
+    public class Inventory
+    {
+        public BindingList<Product> Products { get; }
+        public BindingList<Part> AllParts { get; }
+    }
+
+    public class Product
+    {
+        public BindingList<Part> AssociatedPart { get; }
+        public int ProductID { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int InStock { get; }
+        public int Min { get; }
+        public int Max { get; }
+
+        // FIXME: is a default constructor necessary?
+        // default constructor
+        public Product()
+        {
+
+        }
+
+        // constructor containing all properties
+        public Product(BindingList<Part> associatedPart, int productID, string name, decimal price, int inStock, int min, int max)
+        {
+            AssociatedPart = associatedPart;
+            ProductID = productID;
+            Name = name;
+            Price = price;
+            InStock = inStock;
+            Min = min;
+            Max = max;
+        }
+    }
+
+    public abstract class Part
+    {
+        public int PartID { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int InStock { get; }
+        public int Min { get; }
+        public int Max { get; }
     }
 }
